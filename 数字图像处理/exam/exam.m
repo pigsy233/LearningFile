54ad8e342f68b7c7e785f92ef78306e21beaa99b
pigsy233 <1074993963@qq.com>
2022-01-26 13:37:42 +0800

    daily

--- 数字图像处理/exam/exam.m
+++ 数字图像处理/exam/exam.m
@@ -2,134 +2,46 @@ clc;
 close;
 clear;
 
-slit = imread('10-807J.jpg');
-slit = im2gray(slit);
-slit = imgaussfilt(slit,5);
+slit1 = imread('10-807J.jpg');
+slit1 = im2gray(slit1);
+slit1 = imgaussfilt(slit1,5);
 % slit = imfilter(slit,fspecial('average',[3,3]));
-imhist(slit);
-[Tf SMf] = graythresh(slit);
-Pic2 = im2bw(slit, SMf - 0.2); 
+[Tf1 SMf1] = graythresh(slit1);
 
+slit2 = imread('10-258Z2.jpg');
+slit2 = im2gray(slit2);
+slit2 = imgaussfilt(slit2,5);
+% slit = imfilter(slit,fspecial('average',[3,3]));
+[Tf2 SMf2] = graythresh(slit2);
 
-figure(1)
-subplot(2,2,1);
-imshow(slit);
-title('降噪后图像')
-subplot(222)
-imshow(Pic2)
-title('二值化图像')
+slit3 = imread('10-258Z1.jpg');
+slit3 = im2gray(slit3);
+slit3 = imgaussfilt(slit3,5);
+% slit = imfilter(slit,fspecial('average',[3,3]));
+[Tf3 SMf3] = graythresh(slit3);
+
 
 %用edge算法对二值化图像进行边缘提取
 
-Pic2 = im2bw(slit, SMf - 0.1); 
+Pic1 = im2bw(slit1, SMf1 - 0.15); 
+
+PicEdge1 = edge(Pic1,'sobel',0.4);
+figure();
+imshow(PicEdge1);
+title('10-807J sobel算子');
+
+Pic2 = im2bw(slit2, SMf2 - 0.1625); 
 
 PicEdge2 = edge(Pic2,'sobel',0.1);
 figure();
 imshow(PicEdge2);
-title('sobel算子');
+title('10-258Z2 sobel算子');
 
-PicEdge2 = edge(Pic2,'sobel',0.2);
+Pic3 = im2bw(slit3, SMf3 - 0.2); 
+
+PicEdge3 = edge(Pic3,'sobel',0.2);
 figure();
-imshow(PicEdge2);
-title('sobel算子');
+imshow(PicEdge3);
+title('10-258Z1 sobel算子');
 
-PicEdge2 = edge(Pic2,'sobel',0.3);
-figure();
-imshow(PicEdge2);
-title('sobel算子');
-
-PicEdge2 = edge(Pic2,'sobel',0.4);
-figure();
-imshow(PicEdge2);
-title('sobel算子');
-
-PicEdge2 = edge(Pic2,'sobel',0.5);
-figure();
-imshow(PicEdge2);
-title('sobel算子');
-
-Pic2 = im2bw(slit, SMf - 0.125); 
-
-PicEdge2 = edge(Pic2,'sobel',0.1);
-figure();
-imshow(PicEdge2);
-title('sobel算子');
-
-PicEdge2 = edge(Pic2,'sobel',0.2);
-figure();
-imshow(PicEdge2);
-title('sobel算子');
-
-PicEdge2 = edge(Pic2,'sobel',0.3);
-figure();
-imshow(PicEdge2);
-title('sobel算子');
-
-PicEdge2 = edge(Pic2,'sobel',0.4);
-figure();
-imshow(PicEdge2);
-title('sobel算子');
-
-PicEdge2 = edge(Pic2,'sobel',0.5);
-figure();
-imshow(PicEdge2);
-title('sobel算子');
-
-Pic2 = im2bw(slit, SMf - 0.15); 
-
-PicEdge2 = edge(Pic2,'sobel',0.1);
-figure();
-imshow(PicEdge2);
-title('sobel算子');
-
-PicEdge2 = edge(Pic2,'sobel',0.2);
-figure();
-imshow(PicEdge2);
-title('sobel算子');
-
-PicEdge2 = edge(Pic2,'sobel',0.3);
-figure();
-imshow(PicEdge2);
-title('sobel算子');
-
-PicEdge2 = edge(Pic2,'sobel',0.4);
-figure();
-imshow(PicEdge2);
-title('sobel算子');
-
-PicEdge2 = edge(Pic2,'sobel',0.5);
-figure();
-imshow(PicEdge2);
-title('sobel算子');
-
-% PicEdge1=edge(Pic2,'log');
-% subplot(223);
-% imshow(PicEdge1);
-% title('log算子');
-% 
-% PicEdge2 = edge(Pic2,'canny');
-% subplot(224);
-% imshow(PicEdge2);
-% title('canny算子');
-% 
-% PicEdge3=edge(Pic2,'sobel');
-% figure(2)
-% subplot(221)
-% imshow(PicEdge3);
-% title('sobel算子')
-% 
-% PicEdge4=edge(Pic2,'prewitt');
-% subplot(222)
-% imshow(PicEdge4);
-% title('sprewitt算子')
-% 
-% PicEdge5=edge(Pic2,'zerocross');
-% subplot(223)
-% imshow(PicEdge5);
-% title('zerocross算子')
-% 
-% PicEdge6=edge(Pic2,'roberts');
-% subplot(224)
-% imshow(PicEdge6);
-% title('roberts算子')
 
